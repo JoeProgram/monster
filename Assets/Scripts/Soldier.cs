98738177a501e66ac6a119cf451c35916c881fc3
JoeProgram <joseph.france@gmail.com>
2015-08-22 23:53:49 -0700

    Soldiers now have a jump attack.

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -1,22 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class Soldier : MonoBehaviour {
 
+	public enum SoldierState { WALKING, ATTACKING };
+	public SoldierState soldierState = SoldierState.WALKING;
+
+	public float attackDistance;
+	public float jumpForce;
+
 	protected int health;
 	protected NavMeshAgent agent;
-
+	
 	protected Collider target;
+
+	public GameObject sword;
+
+	protected Tween swordTween;
 
 	void Start(){
 		health = 1;
 		agent = GetComponent<NavMeshAgent> ();
 
 		target = Hydra.instance.GetRandomBodyPart();
+
 	}
 
 	void Update(){
-		agent.SetDestination (target.transform.position);
+
+		if (soldierState == SoldierState.WALKING) {
+			agent.SetDestination (target.transform.position);
+		}
+
+		if (soldierState == SoldierState.WALKING && attackDistance > Vector3.Distance(transform.position,target.transform.position)) {
+			//Debug.Log ( "" + agent.pathStatus + " " + agent.remainingDistance  );
+			//Debug.Log ( "Agents remainin distance to " + target.name + " is " + agent.remainingDistance );
+			StartAttack();
+		} 
+
 	}
 
 	void OnCollisionEnter( Collision collision ){
@@ -24,6 +46,27 @@ public class Soldier : MonoBehaviour {
 		if (collision.gameObject.CompareTag ("hydra") && collision.gameObject.transform.parent.GetComponent<Head>().IsBiting()) {
 			GetHurt();
 		}
+
+		if (soldierState == SoldierState.ATTACKING && collision.gameObject.CompareTag ("floor")) {
+			StartWalk();
+		}
+	}
+
+	public void StartAttack(){
+		soldierState = SoldierState.ATTACKING;
+		agent.enabled = false;
+		swordTween = sword.transform.DOLocalRotate (new Vector3 (0, -83, -20), 0.2f).SetLoops (-1, LoopType.Yoyo);
+		StartCoroutine (AttackJump ());
+	}
+
+	protected IEnumerator AttackJump(){
+		yield return null;
+		GetComponent<Rigidbody> ().AddForce((target.transform.position - transform.position).normalized * jumpForce);
+	}
+
+	public void StartWalk(){
+		soldierState = SoldierState.WALKING;
+		agent.enabled = true;
 	}
 
 	protected void GetHurt(){
